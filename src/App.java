99c6ae8af71900d5d3e688634a6d1ad041239ebf
bmesi <bmesinovic@dmacc.edu>
2018-11-15 21:07:11 -0600

    Week 3 -- Adding List changes to gui. Altered other component in main

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
 
 /**
- * @author Ben Mesi - cis152 - data structurs
- * Main app for project
+ * @author Ben Mesi - cis152 - data structurs Main app for project
  */
 public class App {
 	public static void main(String[] args) {
-		// call gui class
-		GUISTUFF Do = new GUISTUFF();
+					GUI Do = new GUI();
+					Do.frame.setVisible(true);		
+				//	LinkedList a = new LinkedList();
 	}
 }
